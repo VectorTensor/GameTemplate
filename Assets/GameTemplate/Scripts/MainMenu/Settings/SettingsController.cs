3d05626ef767eda3823a3bb60c5c7a5f37bd4f2d
VectorTensor <prayashthapa15@gmail.com>
2024-08-20 19:53:37 +0545

    feat(settings): added settings controller

---
+++ Assets/GameTemplate/Scripts/MainMenu/Settings/SettingsController.cs
@@ -0,0 +1,33 @@
+﻿using GameTemplate.Scripts.MainMenu.Interfaces;
+using UnityEngine;
+
+namespace GameTemplate.Scripts.MainMenu.Settings
+{
+    public class SettingsController : ButtonActionController 
+    {
+        public override void PerformRequiredAction()
+        {
+            
+            //TODO: add logic to start game
+            Debug.Log($"Settings opened");
+            
+        }
+        
+        #region Builder
+
+        public class Builder : GenericBuilder<SettingsController>
+        {
+            public override SettingsController Build()
+            {
+                
+                return new SettingsController
+                {
+                    _model = this._model,
+                    _view = this._view 
+                };
+                
+            }
+        }
+        #endregion
+    }
+}
