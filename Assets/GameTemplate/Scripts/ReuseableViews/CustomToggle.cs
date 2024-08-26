9b3c131d76358bc20ba37ddc442e3d09180c31b3
VectorTensor <prayashthapa15@gmail.com>
2024-08-26 21:14:11 +0545

    feat(toggle): created custom toggle

---
+++ Assets/GameTemplate/Scripts/ReuseableViews/CustomToggle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameTemplate.Scripts.ReuseableViews
+{
+    public class CustomToggle : MonoBehaviour
+    {
+        
+        private ToggleType _toggleType;
+
+        [SerializeField] private GameObject toggleOn;
+
+        [SerializeField] private GameObject toggleOff;
+        public void SetToggleType(ToggleType toggleType)
+        {
+            
+            _toggleType = toggleType;
+            
+            
+        }
+
+        private void SetRequiredImage(ToggleType toggleType)
+        {
+            
+            if (toggleType == ToggleType.On)
+            {
+                
+                toggleOn.SetActive(true);
+                toggleOff.SetActive(false);
+                
+            }
+            else
+            {
+                toggleOn.SetActive(false);
+                toggleOff.SetActive(true);
+                
+            }
+            
+        }
+        
+        
+        
+        
+    }
+    public enum ToggleType
+    {
+        On,
+        Off
+    }
+    
+}
