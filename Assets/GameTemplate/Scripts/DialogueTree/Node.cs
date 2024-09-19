9e4b22a5f7cfa3a53a62ec6a1f1b147a90005e6d
VectorTensor <prayashthapa15@gmail.com>
2024-09-19 09:50:44 +0545

    feat(node): create  node base class

---
+++ Assets/GameTemplate/Scripts/DialogueTree/Node.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace GameTemplate.Scripts.DialogueTree
+{
+    public class Node
+    {
+        public enum Status{Done, Running}
+
+        public readonly string name;
+
+        public readonly List<Node> children = new();
+
+        protected int currentChild;
+
+        public Node(string name = "Node")
+        {
+            this.name = name;
+
+        }
+
+        public void AddChild(Node child) => children.Add(child);
+
+        public virtual Status Process() => children[currentChild].Process();
+
+        public virtual void Reset()
+        {
+
+            currentChild = 0;
+
+            foreach (var child in children)
+            {
+                
+                child.Reset();
+                
+            }
+
+        }
+
+
+
+
+
+    }
+}
