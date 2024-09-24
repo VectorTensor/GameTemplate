6cbaf5d04e4bb0b1bd64719923867f07c2cf4406
VectorTensor <prayashthapa15@gmail.com>
2024-09-24 19:48:25 +0545

    feat(hybrid): added function for creating BotChoiceNode

--- Assets/GameTemplate/Scripts/DialogueTree/Services/HybridNodeService.cs
+++ Assets/GameTemplate/Scripts/DialogueTree/Services/HybridNodeService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using GameTemplate.Scripts.DialogueTree.Strategies;
 using Unity.VisualScripting;
 using UnityEditorInternal;
@@ -28,12 +29,29 @@ namespace GameTemplate.Scripts.DialogueTree.Services
         /// <returns></returns>
         public Sequence GetDi_Node(string name, string dialogue, Func<bool> condition)
         {
-
+            
             Sequence seq = new Sequence(name);
             seq.AddChild(new Leaf(name + " condition", new ConditionStrategy(condition)));
             seq.AddChild(new Leaf(name+" Dialogue" , new DialogueStrategy(dialogue)));
-
             return seq; 
+            
+        }
+        /// <summary>
+        /// Function for getting BotChoiceNode
+        /// </summary>
+        /// <param name="name">Name of the node</param>
+        /// <param name="sequences">List of Di-Node </param>
+        /// <returns></returns>
+        public Selector GetBotChoiceNode(string name , List<Sequence> sequences)
+        {
+
+            Selector sel = new Selector(name);
+            foreach (var seq in sequences)
+            {
+                sel.AddChild(seq);
+            }
+
+            return sel;
 
         }
         
