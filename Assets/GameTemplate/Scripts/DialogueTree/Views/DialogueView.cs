6a98baa97480ef96e9582cc4a643e0daeb111d03
VectorTensor <prayashthapa15@gmail.com>
2024-10-20 18:27:24 +0545

    feat: create dialogue view

---
+++ Assets/GameTemplate/Scripts/DialogueTree/Views/DialogueView.cs
@@ -0,0 +1,22 @@
+﻿using GameTemplate.Scripts.DialogueTree.Models;
+using TMPro;
+using UnityEngine;
+
+namespace GameTemplate.Scripts.DialogueTree.Views
+{
+    public class DialogueView : MonoBehaviour
+    {
+        
+        [SerializeField] private TMP_Text dialogueText;
+        
+        [SerializeField] private TMP_Text nameText;
+        
+
+        public void SetDialogue(DialogueDto dialogue)
+        {
+            dialogueText.text = dialogue.dialogue;
+            nameText.text = dialogue.name;
+        }
+        
+    }
+}
--- Assets/Scripts/DialougeTest/TestDIag.cs
+++ Assets/Scripts/DialougeTest/TestDIag.cs
@@ -14,20 +14,7 @@ namespace DefaultNamespace.DialougeTest
         public bool nt;
         public void Start()
         {
-            
-            
-            
             var root = new DiagTree("root");
-            // var seq1 = new Sequence("seq1");
-            // seq1.AddChild(new Leaf("",new ConditionStrategy(()=> st)));
-            // seq1.AddChild(new Leaf("",new DialogueStrategy("hi")));
-            //
-            // var seq2= new Sequence("seq2");
-            // seq2.AddChild(new Leaf("",new ConditionStrategy(()=> nt)));
-            // seq2.AddChild(new Leaf("",new DialogueStrategy("bye")));
-            // root.AddChild(seq1);
-            // root.AddChild(seq2);
-            // root.Process();
             var d1 = new DialogueDto();
             d1.dialogue = "hi";
             d1.isPlayer = false;
